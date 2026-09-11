81d190e54139769ea1486bfacd2c638b6e7719fc
Mihail <misho.stefanov@gmail.com>
2026-09-11 13:29:19 +0300

    Index page broken symbol fix

--- app/index.js
+++ app/index.js
@@ -2,5 +2,5 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from Azure App Service — deployed by MS!\n');
+    res.end('Hello from Azure App Service - deployed by MS!\n');
 }).listen(port, () => console.log(`Running on port ${port}`));
